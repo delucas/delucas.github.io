58136bc35c0cef6faa42ee7e66764b14b81be898
Lucas Videla <videla.lucas@gmail.com>
2013-07-14 22:52:14 -0300

    Se agrega script de generación de posts

---
+++ _posts/generate_post.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+title = ARGV.join('-').downcase
+date = Time.now.strftime '%Y-%m-%d'
+filename = "#{date}-#{title}.markdown"
+
+File.open(filename, 'w') { |file|
+	file.puts "---"
+	file.puts "layout: post"
+	file.puts "title: '#{ARGV.join(' ')}'"
+	file.puts "date: #{Time.now}"
+	file.puts 'tags:'
+	file.puts '- theTag'
+	file.puts '---'
+	file.puts ''
+	file.puts 'Post body'
+}
